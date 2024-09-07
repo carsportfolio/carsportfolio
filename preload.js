b17682a197ade84a5539bb0324af7ee4cacf74d3
carsportfolio <143631344+carsportfolio@users.noreply.github.com>
2024-09-07 01:58:51 -0600

    Basics V1.6
    push to (almost) finalize the project layout!
    
    added:
    - images for most of the projects in finished or semi-finished state
    - text for all the projects + centering for the clicking and swiping
    - a few links were added to my original websites from college
    - a script to help preload some of the images so the transition is actually executed
    - a border around the description text
    
    fixed:
    - there was padding added to the project-text to fix the spacing in the project details
    
    bugged:
    - the back button (on web)
    - the dot slider is still in the middle of the screen instead of being centered on the text

---
+++ preload.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    preloadPageUsingIframe('project1.html');
+    preloadPageUsingIframe('project2.html');
+    preloadPageUsingIframe('project3.html');
+    preloadPageUsingIframe('project4.html');
+    preloadPageUsingIframe('project5.html');
+    preloadPageUsingIframe('about.html');
+});
+
+function preloadPageUsingIframe(url) {
+    var iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = url;
+
+    iframe.onload = function() {
+        console.log(`Page loaded via iframe: ${url}`);
+    };
+
+    iframe.onerror = function() {
+        console.error(`Error loading page via iframe: ${url}`);
+    };
+
+    document.body.appendChild(iframe);
+}
+
